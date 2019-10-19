f1fe9f7167f11a9388df2bf3b2a264013c2c8a9d
JasmineHJM <965071629@qq.com>
2019-10-19 23:46:37 +0800

    练习

---
+++ 通讯录/通讯录/通讯录/Contact.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Contact.h"
+
+void InitContact(Contact* pcon)
+{
+	assert(pcon);
+	pcon->sz = 0;
+	memset(pcon->data, 0, MAX * sizeof(PeoInfo));
+}
+
+void AddContact(Contact* pcon)
+{
+	assert(pcon);
+	if (pcon->sz == MAX)
+		printf("通讯录已满，无法添加\n");
+	else
+	{
+		printf("请输入名字:>");
+		scanf("%s", pcon->data[pcon->sz].name);
+		printf("请输入年龄:>");
+		scanf("%d", &(pcon->data[pcon->sz].age));
+		printf("请输入性别:>");
+		scanf("%s", pcon->data[pcon->sz].sex);
+		printf("请输入电话:>");
+		scanf("%s", pcon->data[pcon->sz].tele);
+		printf("请输入地址:>");
+		scanf("%s", pcon->data[pcon->sz].addr);
+		pcon->sz++;
+		printf("添加成功\n");
+	}
+}
