7bd9be94f86ec02c72d1e4f93b57a520f147efcc
JasmineHJM <965071629@qq.com>
2019-12-24 21:59:13 +0800

    提交代码

---
+++ learn_Linux/12_24.cpp
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h> 
+#include <fcntl.h>
+#include <unistd.h> 
+#include <string.h>
+/*
+int main()
+{
+	umask(0);
+	int fd = open("myfile", O_WRONLY | O_CREAT, 0644);
+	if (fd < 0){
+		perror("open");
+		return 1;
+	}
+
+	int count = 5;
+	const char *msg = "hello bit!\n";
+	int len = strlen(msg);
+
+	while (count--)
+	{
+		write(fd, msg, len);
+		//fd: 后面讲， msg：缓冲区首地址， len: 本次读取，期望写入多少个字节 的数据。 返回值：实际写了多少字节数据    
+	}
+
+	close(fd);
+	return 0;
+}
+*/
+int main()
+{
+	int fd = open("myfile", O_RDONLY);
+	if (fd < 0){
+		perror("open");
+		return 1;
+	}
+
+	const char *msg = "hello bit!\n";
+	char buf[1024];
+	while (1)
+	{
+		size_t s = read(fd, buf, strlen(msg));//类比write    
+		if (s > 0)
+		{
+			printf("%s", buf);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	close(fd);
+	return 0;
+}
