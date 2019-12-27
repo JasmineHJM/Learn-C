9ba639b918633ae373b695e3876369d69986f672
JasmineHJM <965071629@qq.com>
2019-12-27 22:50:46 +0800

    提交代码

---
+++ learn_Linux/12_27.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void) 
+{
+	int fds[2]; 
+	char buf[100]; 
+	int len;
+
+	if (pipe(fds) == -1)  
+		perror("make pipe"), exit(1);
+
+	// read from stdin  
+	while ( fgets(buf, 100, stdin) ) 
+	{     
+		len = strlen(buf);   
+		// write into pipe     
+		if ( write(fds[1], buf, len) != len ) 
+		{   
+			perror("write to pipe");   
+			break;     
+		}    
+		memset(buf, 0x00, sizeof(buf));       
+		// read from pipe   
+		if ( (len=read(fds[0], buf, 100)) == -1 )
+		{      
+			perror("read from pipe");       
+			break;  
+		}
+		// write to stdout  
+		if ( write(1, buf, len) != len )
+		{      
+			perror("write to stdout");   
+			break;     
+		}  
+	} 
+}
